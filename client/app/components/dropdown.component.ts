f73314eacff9f0b7514ab1b762862c8f693beb69
sanderlenaerts <sander.lenaerts@gmail.com>
2017-04-18 10:32:13 +1200

    Added FAQ and admin link in mobile nav

--- client/app/components/dropdown.component.ts
+++ client/app/components/dropdown.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
     <div id="dropdown" class="dropdown-content">
       <a routerLink="/info"><i class="fa fa-lg fa-fw fa-question-circle"></i>Self-help</a>
       <a routerLink="/live-chat"><i class="fa fa-lg fa-fw fa-comments"></i>Live Chat</a>
+      <a *ngIf="connected && authenticationService.user.role == 'ADMIN'" [routerLink]="['/admin']"><i class="fa fa-lg fa-fw fa-unlock-alt" aria-hidden="true"></i>
+Admin Panel </a>
       <a *ngIf="connected" (click)="logout()"><i class="fa fa-lg fa-fw fa-sign-out"></i>Log out</a>
   </div>
   `,
--- client/app/components/landing.component.ts
+++ client/app/components/landing.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable }     from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'landing',
   template: `
-  
+  <div class="wrapper">
+    <h1>FAQ</h1>
+
+    <div *ngFor="let category of faq">
+      <h2>{{category.display}}</h2>
+      <ul>
+        <li *ngFor="let row of category.faq">
+          <p><i class="fa fa-caret-right" aria-hidden="true"></i>
+  {{row.question}}</p>
+          <p>
+  {{row.answer}}</p>
+        </li>
+      </ul>
+    </div>
+
+    
+  </div>
   `,
-  styleUrls: ['./dist/assets/css/default.css']
+  styleUrls: ['./dist/assets/css/landing.css']
 })
 
-export class LandingComponent {}
+export class LandingComponent implements OnInit {
+
+  faq: any[]
+
+  ngOnInit(){
+    this.getFAQ().subscribe(data => {
+      this.faq = data.help;
+      console.log(data.help)
+    })
+  }
+
+  constructor(private http: Http){
+    
+  }
+
+  getFAQ(): Observable<any> {
+    return this.http.get("../../faq.json")
+      .map((res:any) => res.json());
+  }
+}
